e2fd5c86c1ac42054e484737f488f46d64ee009e
raihanalmenata <raihanalmenata@gmail.com>
2023-12-15 15:49:56 +0700

    Added Footer component

---
+++ src/designs/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Footer = (): JSX.Element => (
+    <footer id="app-footer">
+        <p className="footer-text">
+            Mini_Pokedex is a part of technical test project at Dattabot.id
+        </p>
+    </footer>    
+);
+
+export default Footer;
