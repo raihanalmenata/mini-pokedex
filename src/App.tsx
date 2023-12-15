edfb3ba22998e3113286eaae095b27fa5a7426a4
raihanalmenata <raihanalmenata@gmail.com>
2023-12-15 15:50:23 +0700

    Added Header and Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import './designs/styles/color.css'
 import './designs/styles/typography.css'
 import './designs/styles/other.css'
 
+import Header from './designs/components/Header';
+import Footer from './designs/components/Footer';
+
 const App = (): JSX.Element => {
   return (
     <div id="app-con">
       <div id="app">
+        <Header />
         <div id="app-content">
           { '< App Content >' }
         </div>
+        <Footer />
       </div>
     </div>
   )
