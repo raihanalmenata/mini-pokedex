790df9fd618dc34c98a991c80536350996eaea2e
raihanalmenata <raihanalmenata@gmail.com>
2023-12-17 01:01:30 +0700

    Added data functionality

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -7,8 +7,33 @@ import '../designs/styles/other.css'
 import Header from '../designs/components/Header';
 import Footer from '../designs/components/Footer';
 import Logo from '../designs/components/Logo';
+import PokeCard from './components/PokeCard';
+import PokeInspector from './components/PokeInspector';
+
+import { useCardData } from '../stores/cardData';
+import { getNewCardData } from '../utils/pokemon';
+import { useState } from 'react';
 
 const App = (): JSX.Element => {
+
+  const [cardData, dispatchCardData] = useCardData();
+  const [selectedCardId, setSelectedCardId] = useState('');
+
+  const handleCardListOnScroll = async (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const cardList = e.target as HTMLDivElement;
+    const isScrolledToBottom = Math.abs(cardList.scrollHeight - cardList.clientHeight - cardList.scrollTop) < 1
+
+    if (isScrolledToBottom) {
+      const newCardData = await getNewCardData(7, cardData?.length);
+      dispatchCardData({
+        type: 'added',
+        data: newCardData
+      })
+    }
+  }
+
+  const handleCardClick = (index: string) => setSelectedCardId(index);
+
   return (
     <div id="app-con">
       <div id="app">
@@ -16,14 +41,18 @@ const App = (): JSX.Element => {
           <Logo title={true} />
         </Header>
         <div id="app-content">
-          <div id="card-list">
-            { '< Card List >' }
+          <div id="card-list" 
+          onScroll={handleCardListOnScroll}
+          >
+            {
+              cardData.map(
+                (data) => <PokeCard {...data} key={data.index} clickHandler={handleCardClick}/>
+              )
+            }
           </div>
-          <div id="card-inspector">
-          { '< Card Inspector >' }
-          </div>
+          <PokeInspector selectedCardIndex={selectedCardId}/>
         </div>
-        <Footer 
+        <Footer
           text='Mini_Pokedex is a part of technical test project at Dattabot.id'
         />
       </div>
