eb91b81f023960bf5ef41e3ce7aad7ae35ca1bcd
raihanalmenata <raihanalmenata@gmail.com>
2023-12-16 09:10:38 +0700

    Relocated App component and styles to 'App' dir

---
+++ src/App/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+import '../designs/styles/color.css'
+import '../designs/styles/typography.css'
+import '../designs/styles/other.css'
+
+import Header from '../designs/components/Header';
+import Footer from '../designs/components/Footer';
+
+const App = (): JSX.Element => {
+  return (
+    <div id="app-con">
+      <div id="app">
+        <Header />
+        <div id="app-content">
+          <div id="card-list">
+            { '< Card List >' }
+          </div>
+          <div id="card-inspector">
+          { '< Card Inspector >' }
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
