0831b7ed3d3391abf227a3fb74b148c8057f90b8
raihanalmenata <raihanalmenata@gmail.com>
2023-12-16 10:16:21 +0700

    Added Card component

---
+++ src/designs/components/Card/index.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from 'react';
+import './style.css'
+
+interface Props {
+    title : string,
+    subtitle? : string,
+    description? : string,
+    extraHeader? : string,
+
+    picture? : ReactNode,
+
+    icon? : ReactNode,
+    hideIcon? : boolean
+}
+
+const Card = ({
+    title,
+    subtitle,
+    description,
+    extraHeader,
+    picture,
+    icon,
+    hideIcon
+}: Props): JSX.Element => (
+    <div className="card">
+        <div className="card-pic-con">
+            {
+                picture && picture
+            }
+        </div>
+        <div className="card-data-con">
+            <h2 className="extra-header">
+                { extraHeader }
+            </h2>
+            <h1 className="title">
+                { title }
+            </h1>
+            <h3 className="subtitle">
+                { subtitle }
+            </h3>
+            <h4 className="description">
+                { description }
+            </h4>
+        </div>
+        <div className={'card-icon-con' + (hideIcon && 'hide')}>
+            {
+                icon && icon
+            }
+        </div>
+    </div>
+);
+
+export default Card;
