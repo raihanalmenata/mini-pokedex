c9f74db3a0d5f36f6bbe645c88f1d218a51ed14f
raihanalmenata <raihanalmenata@gmail.com>
2023-12-16 18:53:08 +0700

    Added pokemon adapter

---
+++ src/adapters/pokemon.ts
@@ -0,0 +1,37 @@
+const sourceBaseUrl = 'https://pokeapi.co/api/v2';
+
+    export const getPokemonUrl = async (limit: number, offset: number) => {
+        const endpoint = '/pokemon';
+        const query = '?' + new URLSearchParams({'limit' : limit.toString(), 'offset': offset.toString()}).toString();
+        const reqUrl = sourceBaseUrl + endpoint + query;
+
+        const reqData = {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            }
+        }
+
+        const response =  await fetch(reqUrl, reqData);
+        const json = await response.json();
+
+
+        return json['results'];
+    }
+
+    export const getPokemonDetail = async (index: string) => {
+        const endpoint = '/pokemon/' + index;
+        const reqUrl = sourceBaseUrl + endpoint
+
+        const reqData = {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            }
+        }
+
+        const response =  await fetch(reqUrl, reqData)
+        const json = await response.json()
+
+        return json;
+    }
